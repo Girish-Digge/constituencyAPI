692673565f2b5995946afc77cf8c850338c25995
Girish Digge <girishdigge@gmail.com>
2023-10-03 23:24:33 +0530

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 require("express-async-errors");
 
-const path = require("path");
+// const path = require("path");
 //extra security packages
 const helmet = require("helmet");
 // const cors = require("cors");
@@ -21,23 +21,23 @@ const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 app.set("trust proxy", 1);
 
-app.use(express.static(path.resolve(__dirname, "./client/dist")));
+// app.use(express.static(path.resolve(__dirname, "./client/dist")));
 app.use(express.json());
 app.use(helmet());
 // app.use(cors());
 app.use(xss());
 
-// routes
-// app.get("/", (req, res) => {
-//   res.send("constituency api");
-// });
+// routes;
+app.get("/", (req, res) => {
+  res.send("constituency api");
+});
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/complaints", authenticateUser, complaintsRouter);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
+// });
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
